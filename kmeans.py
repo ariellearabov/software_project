0b8d7439277a0859308f82c8ccc416d0decc1bb7
arabov <arabov@mail.tau.ac.il>
2022-12-14 19:49:16 +0200

    added methods to calculate the kmeans algorithm

--- kmeans.py
+++ kmeans.py
@@ -7,7 +7,7 @@ import sys
 
 def main():
     try:
-        input_file_path = check_input()
+        input_file_path, iter_arg = check_input()
     except(AssertionError):
         print("An Error Has Occurred")
         return 
@@ -22,10 +22,15 @@ def main():
         return 
 
     try:
-        iter = validation_iter()
+        iter = validation_iter(iter_arg)
     except(AssertionError):
         print("Invalid maximum iteration!")
         return 
+    
+    try:
+        clusters = calc_k_means(k, iter, mat)
+    except:
+        print("An Error Has Occurred")
     
 
     return 0
@@ -47,28 +52,30 @@ def read_file(file_path):
     return mat
 
 '''
-This function checks if the input is valid   
+This function checks if the input is valid, if everything is OK returns the files' path  
 '''
 
 def check_input():
     n = len(sys.argv)
     assert n in (3, 4) # checks validity of input
     i = 1
+    iter_arg = False
 
     if n == 4: # iter included in input
         i += 1
+        iter_arg = True
     
-    input_file_path = sys.argv[i + 1] # If iter value is not a part of input, this will be 2nd arg
-    return input_file_path
+    input_file_path = sys.argv[i + 1] # If iter value is not a part of input, this will be 2nd arg 
+    return input_file_path, iter_arg
 
 '''
 validation_x functions check whether the specific args are valid 
 '''
 
-def validation_k(N):  # need to change this so it will work 
+def validation_k(N):  
     try:
-        k = int(sys.argv[1])
-        if not 1<k<N:
+        k = int(sys.argv[1])  # checks if k arg is an int 
+        if not 1<k<N:  # checks if k in valid range 
             assert 1 == 0
     except:
         assert 1 == 0
@@ -76,15 +83,53 @@ def validation_k(N):  # need to change this so it will work
         return k
 
 
-def validation_iter():  # need to change this so it will work 
+def validation_iter(iter_arg): 
     try:
-        iter = int(sys.argv[2])
-        if not 1<iter<1000:
-            assert 2 == 1
+        if iter_arg:  # if iter arg exists 
+            iter = int(sys.argv[2])  # checks if k arg is an int 
+            if not 1<iter<1000:  # checks if iter in valid range 
+                assert 2 == 1
     except:
         assert 2 == 1
-    else:
-        return iter 
+    if not iter_arg:
+        iter = 200  # no arg given, gives defualt arg as stated 
+    return iter 
 
+'''
+This function calculates the K cluster centroids produced by the K-means algorithm.
+Firstly, the function initializes the centroids to be the first k datapoints.
+Then the function performs the following-
+- Add each observation to its closest cluster and removing it from its old one. 
+  Finding the closet cluster is done using the "find_closest" function.
+- Calculat each cluster's new centroid (as the average of the cluster's updated observations).
+- Calculat each cluster's *deviation* between its old centroid and its new one. 
+  The deviation is calculated using the "square_euclidean_distance" function. 
+The function stops when iter iterations were preformed, or when the deviation of all the clusters 
+is less than epsilon squared (i.e., the distance itself is less than epsilon).
+'''
+def calc_k_means(k, iter, mat, epsilon=0.001):
+    centroids = [mat[i] for i in range(k)] # Initialize centroids as first k datapoints
+    converged = False 
+    while (iter > 0) and (not converged):
+        for x in mat: # checks each dataset 
+            i = 0 # just so threr wont be errors. 
+
+    return 0
+
+'''
+This function finds the index of the closest centroid to dataset x.
+Distance is measured with euclidean distance. 
+Assumptions - for each centroid dimension(x) == dimension(centorid).
+The function uses the euclidean_distance function. 
+'''
+def find_closest_cluster(x, clusters):
+    minimal_index = 0
+    min_dist = euclidean_distance()  # need to continue this func 
+
+'''
+'''
+def euclidean_distance():  # need to wrtie this func 
+    return 0
+    
 if __name__ == "__main__":
     main() 
